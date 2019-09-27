6a427eae333756b03977b9aa5be537f1b3687d27
David Timms <datimms@gmail.com>
2019-09-27 22:14:40 +0100

    improvements

--- storage.js
+++ storage.js
@@ -1,10 +1,27 @@
-if(localStorage.length>0) {
-    //value is stored
-  } else {
-    //storage is empty
-  }
-  
-  
-function setValue() {
-    
+
+var sw = false;
+var strEmpty = "You have not generated any links yet.";
+var strFull = "The list is winning";
+
+// var list = document.getElementById("list");
+// var newp = document.createElement("p");
+//     newp.innerHTML = strEmpty;
+//     list.appendChild(newp);
+
+
+
+
+
+if (sw == true) {
+  var newp = document.createElement("p");
+  newp.innerHTML = strEmpty;
+  list.appendChild(newp);
 }
+else {
+  var newp = document.createElement("p");
+  newp.innerHTML = strFull;
+  list.appendChild(newp);
+}
+  
+
+//function setValue() {
